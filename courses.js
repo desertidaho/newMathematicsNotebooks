a6ba85d23433aa1f3ce7dd13b709e81068101f8f
desertidaho <wilcox.brett@gmail.com>
2021-03-03 14:58:14 -0700

    Move function to courses.js

---
+++ courses.js
@@ -0,0 +1,106 @@
+function change(course) {
+    if (course == 143) {
+      course = "1MczMsDICd\-Y9CLX3CDvVnBxuE\_uFL\_KY"
+    }
+    else if (course == 144) {
+      course = "1C6JgPUZ9Qsq6HhM665NQRq_58CkbW8zb"
+    }
+    else if (course == 170) {
+      course = "1z-RZL3B5ZeGjmgluIIghCKY38A0DG8J1"
+    }
+    else if (course == 175) {
+      course = "1-jVQMe_UJpKOmK43W3SGxesgc31vmdBD"
+    }
+    else if (course == 2751) {
+      course = "1gv7b8gbX35o_EcyI9SJZ_luNFxy14tZM"
+    }
+    else if (course == 2752) {
+      course = "19yimVaYT8b7ygDoR1oVsy9BwjATJLWxW"
+    }
+    else if (course == 287) {
+      course = "11OnKjV_p8WmB9UEv0rXqoW7eup9Ejg2e"
+    }
+    else if (course == 301) {
+      course = "18zUwEuIOQcqktCXmJaGTN4dtAwCkva-1"
+    }
+    else if (course == 3011) {
+      course = "1eMDL3k81bnGufLIc7U6KoeCIkt1rPo0d"
+    }
+    else if (course == 3012) {
+      course = "1BkSB5KrrFDtRhstaeqhfQ1bu0yKvoJr-"
+    }
+    else if (course == 3013) {
+      course = "1XmEbVVmv18_T-_PjkWIvsUueJqx09YTE"
+    }
+    else if (course == 314) {
+      course = "1TY8zuDh1oi5RxtjZTdsmS492_R8zC9yf"
+    }
+    else if (course == 333) {
+      course = "1l4SI44ev3ElwPK8DKZBWbFg9QnpOh4zO"
+    }
+    else if (course == 3331) {
+      course = "193rgf47qmyEk9xnsDhixPhByzcYk8JBS"
+    }
+    else if (course == 3332) {
+      course = "1J17-eahV1lnEONnleNFa3-pBgOf9o2_7"
+    }
+    else if (course == 361) {
+      course = "1XZPpRHFySTnlYJnwzfNxYoGoP4-bjfDl"
+    }
+    else if (course == 3611) {
+      course = "1tpc6xmjlNb5cnlTEiGTa-3kIzTBmSnRK"
+    }
+    else if (course == 3612) {
+      course = "1xbwOJARN8EDPyDt1ESbhARnwV2EhGg_v"
+    }
+    else if (course == 365) {
+      course = "1Elj0ippFo6I6XXopugNqCqVur-XsKQj1"
+    }
+    else if (course == 3651) {
+      course = "1lZJWFUmQnU623SEbF0rwtNAnzb67iwea"
+    }
+    else if (course == 403) {
+      course = "1PxLfc1G9DZRV96osinaIml0kaGe0t8CZ"
+    }
+    else if (course == 4031) {
+      course = "1T6Ezmvp_A-XOGQoSjG4sKIDVY_iB4RZw"
+    }
+    else if (course == 4032) {
+      course = "1ojAh1S7VZnfxWppEyLMDSuxRcM6HElBj"
+    }
+    else if (course == 427) {
+      course = "1gKdO0dtxaXqzdlnmK2EuqZeGV_MIsNX6"
+    }
+    else if (course == 4271) {
+      course = "1X2osR7lxCstbqNKhiiv8xB9t6hAG3weS"
+    }
+    else if (course == 4272) {
+      course = "1kdsJsz9ntNo1VAr3rDK6onn-1Eg6YLG8"
+    }
+    else if (course == 436) {
+      course = "1fi_ljnNnxqyztTWZHMbRyvlBraOIFEM9"
+    }
+    else if (course == 4361) {
+      course = "1Gf0JusamqMk_VcFlKc9tysv_X5d6aNpi"
+    }
+    else if (course == 4362) {
+      course = "1l_laEdKswLZzetQN6-ykywYUXttuls1K"
+    }
+    else if (course == 465) {
+      course = "1TEqOvZ5w6_YzQmbd-lGASO26MRi6oWp-"
+    }
+    else if (course == 4651) {
+      course = "1RYWhGvuVA15hOXxExhGHszLT_0IUdBlt"
+    }
+    else if (course == 4652) {
+      course = "1gK4EeVMnfrxO5IHyMlZZEKSmowvXRqsv"
+    }
+    else if (course == 4653) {
+      course = "1VuGp1nmxQS3-7s7XIbsRfuJfSZjn48OY"
+    }
+    else if (course == 999) {
+      course = "0BzhRKR0oJlTnNDdZSW1naXdudmRVRTJwUEZTVjQ3b2hQcmMw"
+    }
+
+    document.getElementById('pdf').innerHTML = `<iframe src="https://drive.google.com/file/d/${course}/preview" height="1075px" width="90%"></iframe>`
+  }
