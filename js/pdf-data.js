da7e1a25cc9345ffffb3ea67d784bf5ca4cfa212
desertidaho <wilcox.brett@gmail.com>
2021-03-02 20:23:02 -0700

    Add courses

--- js/pdf-data.js
+++ js/pdf-data.js
@@ -1,37 +1,4 @@
 export default class PdfData {
-
-    // 2751 = `https://drive.google.com/file/d//preview`
-    // 2752 = `https://drive.google.com/file/d/19yimVaYT8b7ygDoR1oVsy9BwjATJLWxW/preview`
-    // 287 = `https://drive.google.com/file/d/11OnKjV_p8WmB9UEv0rXqoW7eup9Ejg2e/preview`
-    // 301 = `https://drive.google.com/file/d/18zUwEuIOQcqktCXmJaGTN4dtAwCkva-1/preview`
-    // h301 = `https://drive.google.com/file/d/1eMDL3k81bnGufLIc7U6KoeCIkt1rPo0d/preview`
-    // a301 = `https://drive.google.com/file/d/1BkSB5KrrFDtRhstaeqhfQ1bu0yKvoJr-/preview`
-    // t301 = `https://drive.google.com/file/d/1XmEbVVmv18_T-_PjkWIvsUueJqx09YTE/preview`
-    // 314 = `https://drive.google.com/file/d/1TY8zuDh1oi5RxtjZTdsmS492_R8zC9yf/preview`
-    // 333 = `https://drive.google.com/file/d/1l4SI44ev3ElwPK8DKZBWbFg9QnpOh4zO/preview`
-    // q333 = `https://drive.google.com/file/d/193rgf47qmyEk9xnsDhixPhByzcYk8JBS/preview`
-    // t333 = `https://drive.google.com/file/d/1J17-eahV1lnEONnleNFa3-pBgOf9o2_7/preview`
-    // 361 = `https://drive.google.com/file/d/1XZPpRHFySTnlYJnwzfNxYoGoP4-bjfDl/preview`
-    // h361 = `https://drive.google.com/file/d/1tpc6xmjlNb5cnlTEiGTa-3kIzTBmSnRK/preview`
-    // t361 = `https://drive.google.com/file/d/1xbwOJARN8EDPyDt1ESbhARnwV2EhGg_v/preview`
-    // 365 = `https://drive.google.com/file/d/1Elj0ippFo6I6XXopugNqCqVur-XsKQj1/preview`
-    // h365 = `https://drive.google.com/file/d/1lZJWFUmQnU623SEbF0rwtNAnzb67iwea/preview`
-    // 403 = `https://drive.google.com/file/d/1PxLfc1G9DZRV96osinaIml0kaGe0t8CZ/preview`
-    // h403 = `https://drive.google.com/file/d/1T6Ezmvp_A-XOGQoSjG4sKIDVY_iB4RZw/preview`
-    // t403 = `https://drive.google.com/file/d/1ojAh1S7VZnfxWppEyLMDSuxRcM6HElBj/preview`
-    // 427 = `https://drive.google.com/file/d/1gKdO0dtxaXqzdlnmK2EuqZeGV_MIsNX6/preview`
-    // h427 = `https://drive.google.com/file/d/1X2osR7lxCstbqNKhiiv8xB9t6hAG3weS/preview`
-    // t427 = `https://drive.google.com/file/d/1kdsJsz9ntNo1VAr3rDK6onn-1Eg6YLG8/preview`
-    // 436 = `https://drive.google.com/file/d/1fi_ljnNnxqyztTWZHMbRyvlBraOIFEM9/preview`
-    // h436 = `https://drive.google.com/file/d/1Gf0JusamqMk_VcFlKc9tysv_X5d6aNpi/preview`
-    // t436 = `https://drive.google.com/file/d/1l_laEdKswLZzetQN6-ykywYUXttuls1K/preview`
-    // 465 = `https://drive.google.com/file/d/1TEqOvZ5w6_YzQmbd-lGASO26MRi6oWp-/preview`
-    // h465 = `https://drive.google.com/file/d/1RYWhGvuVA15hOXxExhGHszLT_0IUdBlt/preview`
-    // q465 = `https://drive.google.com/file/d/1gK4EeVMnfrxO5IHyMlZZEKSmowvXRqsv/preview`
-    // t465 = `https://drive.google.com/file/d/1VuGp1nmxQS3-7s7XIbsRfuJfSZjn48OY/preview`
-    // senior = `https://drive.google.com/file/d/0BzhRKR0oJlTnNDdZSW1naXdudmRVRTJwUEZTVjQ3b2hQcmMw/preview`
-
-
   pdfData() {
     let data =
         [
@@ -39,7 +6,36 @@ export default class PdfData {
             { course: 144, title: "Math 144", description: "Analytical Trigonomotry" },
             { course: 170, title: "Math 170", description: "Calculus I" },
             { course: 175, title: "Math 175", description: "Calculus II" },
-            { course: 2751, title: "Math 275", description: "Multivariable and Vector Calculus, part I" }
+            { course: 2751, title: "Math 275", description: "Multivariable and Vector Calculus, part I" },
+            { course: 2752, title: "Math 275", description: "Multivariable and Vector Calculus, part II" },
+            { course: 287, title: "Math 287", description: "Communications in Mathematics" },
+            { course: 301, title: "Math 301", description: "Introduction to Linear Algebra" },
+            { course: 3011, title: "Math 301 Homework", description: "Introduction to Linear Algebra" },
+            { course: 3012, title: "Math 301 Activities", description: "Introduction to Linear Algebra" },
+            { course: 3013, title: "Math 301 Tests", description: "Introduction to Linear Algebra" },
+            { course: 314, title: "Math 314", description: "Foundations of Analysis" },
+            { course: 333, title: "Math 333", description: "Differential Equations with Matrix Theory" },
+            { course: 3331, title: "Math 333 Quizzes", description: "Differential Equations with Matrix Theory" },
+            { course: 3332, title: "Math 333 Tests", description: "Differential Equations with Matrix Theory" },
+            { course: 361, title: "Math 361", description: "Probability and Statistics" },
+            { course: 3611, title: "Math 361 Homework", description: "Probability and Statistics" },
+            { course: 3612, title: "Math 361 Tests", description: "Probability and Statistics" },
+            { course: 365, title: "Math 365", description: "Introduction to Computational Math" },
+            { course: 3651, title: "Math 365 Homework", description: "Introduction to Computational Math" },
+            { course: 403, title: "Math 403", description: "Linear Algebra" },
+            { course: 4031, title: "Math 403 Homework", description: "Linear Algebra" },
+            { course: 4032, title: "Math 403 Tests", description: "Linear Algebra" },
+            { course: 427, title: "Math 427", description: "Applied Mathematics for Scientists and Engineers" },
+            { course: 4271, title: "Math 427 Homework", description: "Applied Mathematics for Scientists and Engineers" },
+            { course: 4272, title: "Math 427 Tests", description: "Applied Mathematics for Scientists and Engineers" },
+            { course: 436, title: "Math 436", description: "Partial Differential Equations" },
+            { course: 4361, title: "Math 436 Homework", description: "Partial Differential Equations" },
+            { course: 4362, title: "Math 436 Tests", description: "Partial Differential Equations" },
+            { course: 465, title: "Math 465", description: "Numerical Methods" },
+            { course: 4651, title: "Math 465 Homework", description: "Numerical Methods" },
+            { course: 4652, title: "Math 465 Quizzes", description: "Numerical Methods" },
+            { course: 4653, title: "Math 465 Tests", description: "Numerical Methods" },
+            { course: 999, title: "Thesis", description: "Boise State University Thesis" }
         ]
     return data
     } 
